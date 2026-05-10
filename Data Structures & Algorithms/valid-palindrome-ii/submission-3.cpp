8de1e22912696ed8f9b423befa2a1bc093598184
Pratyay Guchhait <52366827+GuchhaitPratyay@users.noreply.github.com>
2026-05-10 22:21:04 +0530

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    bool isPalindrome(const string& s, int start, int end) {
+        while (start < end) {
+            if (s[start] != s[end]) return false;
+            start++;
+            --end;
+        }
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s) {
+        int start = 0, end = s.length()-1;
+        while (start < end) {
+            if (s[start] != s[end]) {
+                return isPalindrome(s, start+1, end) || isPalindrome(s, start, end-1);
+            }
+            ++start;
+            --end;             
+        }
+        return true;
+    }
+};
