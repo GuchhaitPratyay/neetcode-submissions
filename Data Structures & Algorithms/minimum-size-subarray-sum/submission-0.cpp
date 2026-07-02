436cce567e4502e5ffc146802214d736db50d282
Pratyay Guchhait <52366827+GuchhaitPratyay@users.noreply.github.com>
2026-07-02 06:10:59 +0530

    Add: minimum-size-subarray-sum - submission-0

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int start = 0, end = 0;
+        int runningSum = 0;
+        int minWindow = INT32_MAX;
+
+        while (end < nums.size()) {
+            runningSum += nums[end];
+            if (runningSum >= target) {
+                while (runningSum >= target) {
+                    minWindow = min(minWindow, (end-start+1));
+                    runningSum -= nums[start++];  
+                }                   
+            }
+            ++end;            
+        }
+
+        return minWindow == INT32_MAX ? 0 : minWindow;
+    }
+};
