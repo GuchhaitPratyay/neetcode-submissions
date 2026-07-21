d43e23fb2f63321330a91bc29c0a595ff9ed1a6a
Pratyay Guchhait <52366827+GuchhaitPratyay@users.noreply.github.com>
2026-07-21 15:42:42 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int max_profit = 0;
+
+        for (int i = 0; i < prices.size(); ++i) {
+            for (int j = i+1; j <prices.size(); ++j) {
+                int diff = prices[j] - prices[i];
+                if (diff > max_profit) max_profit = diff;
+            }
+        }
+        return max_profit;
+    }
+};
