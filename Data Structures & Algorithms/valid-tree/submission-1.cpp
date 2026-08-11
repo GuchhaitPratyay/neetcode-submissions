c2586be7cf92eacdeb8c3653131078afd7d2b3cc
Pratyay Guchhait <52366827+GuchhaitPratyay@users.noreply.github.com>
2026-08-11 23:00:52 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+private:
+    bool dfs(int source, int parent, vector<vector<int>> &adjacencyList, vector<bool> &visitedNodes) {
+        visitedNodes[source] = true;
+
+        for (auto& neighbour : adjacencyList[source]) {
+            if (!visitedNodes[neighbour]) {
+                dfs(neighbour, source, adjacencyList, visitedNodes);
+            } else {
+                if (neighbour != parent) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        if (edges.size() < n-1) return false;
+        bool isValidTree = false;
+        //prepare a adjacency list to represent a graph
+        vector<vector<int>> adjacencyList(n);
+
+        for (int i = 0; i < edges.size(); ++i) {
+            adjacencyList[edges[i][0]].push_back(edges[i][1]);
+            adjacencyList[edges[i][1]].push_back(edges[i][0]);
+        }
+
+        vector<bool> visitedNodes(n);
+
+        for (int i = 0; i < n; ++i) {
+            if (!visitedNodes[i]) {
+                isValidTree = dfs(i, -1, adjacencyList, visitedNodes);
+                if (!isValidTree) return false;
+            }            
+        }
+
+        return true;
+    }
+};
