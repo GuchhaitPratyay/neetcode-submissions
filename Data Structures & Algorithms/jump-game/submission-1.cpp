9876e015e8d9369c544b531b272d4e49bb9263d2
Pratyay Guchhait <52366827+GuchhaitPratyay@users.noreply.github.com>
2026-08-19 12:45:26 +0530

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int position = 0;
+
+        while (position < nums.size()-1) {
+            if (nums[position] == 0) {
+                return false;
+            }
+            position += nums[position];
+        }
+
+        return position >= nums.size()-1 ? true : false;
+    }
+};
