cd52027eb5f45775aef2c214ea853f0af0abe38c
Pratyay Guchhait <52366827+GuchhaitPratyay@users.noreply.github.com>
2026-08-19 10:42:08 +0530

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int lemonadePrice = 5;
+        int amountWithShopKeeper = 0;
+
+        for (auto& bill : bills) {
+            if (amountWithShopKeeper < 0) {
+                return false;
+            }
+            if (bill == lemonadePrice) {
+                amountWithShopKeeper += bill;
+            } else {
+                //given that bills can be only 5, 10, 20
+                int returnAmount = bill - lemonadePrice;
+                if (returnAmount > amountWithShopKeeper) {
+                    return false;
+                } else {
+                    amountWithShopKeeper = (amountWithShopKeeper - returnAmount) + bill;
+                }
+            }
+        }
+
+        return true;
+    }
+};
