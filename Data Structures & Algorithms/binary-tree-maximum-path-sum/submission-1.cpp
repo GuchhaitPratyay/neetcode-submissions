0019f9b71ecd3680396b0b7687b7fde7459e8307
Pratyay Guchhait <52366827+GuchhaitPratyay@users.noreply.github.com>
2026-08-22 20:23:26 +0530

    Add: binary-tree-maximum-path-sum - submission-1

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-1.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+    //int getMaxPathSum(TreeNode* root) {
+    //    if (root == nullptr) {
+    //        return 0;
+    //    }
+//
+    //    int leftBest = getMaxPathSum(root->left);
+    //    int rightBest = getMaxPathSum(root->right);
+//
+    //    int bestPath = root->val + max(leftBest, rightBest);
+    //    
+    //    return max(0, bestPath);
+    //}
+
+    int dfs(TreeNode* root, int& result) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        //here we are calculating left and right maximum
+        int leftBest = max(dfs(root->left, result), 0);
+        int rightBest = max(dfs(root->right, result), 0);
+
+        result = max(result, root->val + leftBest + rightBest);
+
+        return root->val + max(leftBest, rightBest);
+    }
+public:
+    //int result = INT32_MIN;
+    int maxPathSum(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int result = root->val;
+        //Perform DFS traversal 
+        dfs(root, result);
+        return result;
+    }
+};
